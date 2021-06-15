9b7f764def3356952f3886b9130f56418b5a5029
worakrit <worakrit.dev@gmail.com>
2021-06-15 07:48:48 +0700

    main

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd612ccf0539529c5b58d53292ec60532'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1f069c1a-c0b5-4393-96a0-da1be443f9b8'
+};
